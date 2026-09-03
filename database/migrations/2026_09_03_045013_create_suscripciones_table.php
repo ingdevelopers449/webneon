3f3f300bf2dcbb536cb3f4aae73d0876615ff34e
ingdevelopers449 <pipelozada994@gmail.com>
2026-09-02 23:54:02 -0500

    Creacion de la tabla subscripciones y el modelo sin configurar

---
+++ database/migrations/2026_09_03_045013_create_suscripciones_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('suscripciones', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('usuario_id')->constrained('usuarios_sistema');
+            $table->date('fecha_inicio');
+            $table->date('fecha_vencimiento');
+            $table->integer('dias_restantes')->nullable();
+            $table->timestamps(); // Crea fecha_creacion (created_at) y fecha_actualizacion (updated_at)
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('suscripciones');
+    }
+};
